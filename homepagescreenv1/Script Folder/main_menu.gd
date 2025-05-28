8e3a1123697a4adee88bc7e5a7020fcb46ba81e1
The Rose Coder <dlarose573@outlook.com>
2025-05-28 18:29:29 -0400

    Update main_menu.gd

--- homepagescreenv1/Script Folder/main_menu.gd
+++ homepagescreenv1/Script Folder/main_menu.gd
@@ -13,7 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_start_pressed():
 	print("Start pressed")
-	get_tree().change_scene_to_file("res://world.tscn")
+	get_tree().change_scene_to_file("res://Scenes/world.tscn")
 
 
 func _on_options_pressed():
